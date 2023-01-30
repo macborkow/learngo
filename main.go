b126a5887230e4b443e655746c020ea2bd562218
Maciej Borkowski <maciej.borkowski@nl.abnamro.com>
2023-01-30 11:13:42 +0100

    static site

--- main.go
+++ main.go
@@ -14,5 +14,8 @@ func main() {
     fmt.Fprintf(w, "hello there!")
   })
 
+  fs := http.FileServer(http.Dir("static/"))
+  http.Handle("/static/", http.StripPrefix("/static/", fs))
+
   http.ListenAndServe(":1234", nil)
 }
