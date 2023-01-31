6989ec367923eddd98c15af2de700a0e631b0c08
Maciej Borkowski <maciej.borkowski@nl.abnamro.com>
2023-01-31 15:10:21 +0100

    add mysql

--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module example/web
+module example.com/web/v2
 
 go 1.19
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.0
+	github.com/gorilla/mux v1.8.0
+)
--- main.go
+++ main.go
@@ -3,11 +3,71 @@ package main
 import (
   "fmt"
   "net/http"
+  "log"
+  "time"
 
   "github.com/gorilla/mux"
+  "database/sql"
+  _ "github.com/go-sql-driver/mysql"
 )
 
+
 func main() {
+  db, err := sql.Open("mysql", "root:root@(172.17.0.2:3306)/mysql?parseTime=true")
+  if err != nil {
+    log.Fatal(err)
+  }
+  if err := db.Ping(); err != nil {
+    log.Fatal(err)
+  }
+
+/*
+  { // create a table
+    query := `
+      CREATE TABLE users (
+        id INT AUTO_INCREMENT,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL,
+        created_at DATETIME,
+        PRIMARY KEY (id)
+      );`
+
+    if _, err := db.Exec(query); err != nil {
+      log.Fatal(err)
+    }
+  }
+*/
+
+  { // insert a user
+    username := "john"
+    password := "secret"
+    createdAt := time.Now()
+
+    result, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`, username, password, createdAt)
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    id, err := result.LastInsertId()
+    fmt.Println(id)
+  }
+
+  { // query a single user
+    var (
+      id        int
+      username  string
+      password  string
+      createdAt time.Time
+    )
+
+    query := "SELECT id, username, password, created_at FROM users WHERE id = ?"
+    if err := db.QueryRow(query, 1).Scan(&id, &username, &password, &createdAt); err != nil {
+      log.Fatal(err)
+    }
+
+    fmt.Println(id, username, password, createdAt)
+  }
+
   r := mux.NewRouter()
 
   r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
